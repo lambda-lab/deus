593149cba27b0789d1dae0e1adf064ea4908a30b
Stefan Nožinić <stefan@lugons.org>
2021-02-01 22:28:06 +0100

    Restructure code

--- test/runTests.sh
+++ test/runTests.sh
@@ -1,7 +1,16 @@
 #!/bin/sh 
 
+# add here list of directories under ./src that should be linked to tests 
+LINK_SOURCE_CODE_DIRECTORIES=""
+
+
 cd $(dirname $(readlink -f $0))
 
-gcc *.c -lcunit -o runner -I../src/
+SOURCE_FILES=""
+for directory in $LINK_SOURCE_CODE_DIRECTORIES; do 
+    SOURCE_FILES="$SOURCE_FILES ../src/$directory/*.c"
+done
+
+gcc $SOURCE_FILES *.c -lcunit -o runner -I../src/
 ./runner
 rm runner
